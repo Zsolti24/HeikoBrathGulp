aa5d933e2b6e92a08c120addf13c4adf1d1c12ca
Zsolti24 <tothzsolt2004@gmail.com>
2024-07-30 10:59:05 +0200

    ReservationSection

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// // import Swiper from 'swiper';
-
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     loop: true,
-  
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-  
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-  
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-//   });
-
 document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper('.swiper', {
 direction: 'horizontal',
@@ -38,3 +14,10 @@ scrollbar: {
 },
 });
 });
+
+function swapImage(thumbnail) {
+    const mainImage = document.getElementById('mainImage');
+    const tempSrc = mainImage.src;
+    mainImage.src = thumbnail.src;
+    thumbnail.src = tempSrc;
+}

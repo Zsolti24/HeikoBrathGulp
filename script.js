87ee3f666b0c0268a3bc56f6e05aaeba29d896e8
Zsolti24 <tothzsolt2004@gmail.com>
2024-07-30 10:53:42 +0200

    Swiper works without design, Description Section

--- script.js
+++ script.js
@@ -21,3 +21,20 @@
 //       el: '.swiper-scrollbar',
 //     },
 //   });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+direction: 'horizontal',
+loop: true,
+pagination: {
+    el: '.swiper-pagination',
+},
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
+scrollbar: {
+    el: '.swiper-scrollbar',
+},
+});
+});
